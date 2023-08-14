a97d7f37015b605321694f05fcc8a00f28b7ab60
Oleg Borisov <oleg.borisov@uniklinik-freiburg.de>
2023-08-14 15:51:24 +0200

    FinnGen

--- R/coloc_tmp.R
+++ R/coloc_tmp.R
@@ -1,1 +1,29 @@
 # test code
+#' @title Query UKB GWAS
+#' @description Query UK Biobank GWAS data to extract a region of interest
+#' @param sumstats_file path to UKB sumstats.
+#' @param annotation_file annotation file with number of samples for each GWAS.
+#' @param CHR_var CHR (as.character "1", "2", ..., "X").
+#' @param BP_START_var start of region, integer
+#' @param BP_STOP_var end of region, integer
+#' @return data frame with extracted sumstats.
+#' @export
+
+
+query_finngen_GWAS <- function(sumstats_file,
+                           CHR_var, BP_START_var, BP_STOP_var) {
+  sumstats <- read.delim(text=system(paste0("tabix -h ", sumstats_file, " ",
+                                            CHR_var, ":", BP_START_var, "-",
+                                            BP_STOP_var), intern = T),  header = T)
+  
+  if (nrow(sumstats) == 0) { return(NA) }
+  # format
+  sumstats$Name <- paste0("chr", sumstats$X.chrom, ":", sumstats$pos, ":", sumstats$ref, ":", sumstats$alt)
+  sumstats <- sumstats[,c("Name", "rsids", "X.chrom", "pos", "alt", "ref", "beta", "sebeta", "pval", "af_alt")]
+  colnames(sumstats) <- c("Name", "rsID", "CHR", "POS", "A1", "A2", "BETA", "SE", "P", "AF")
+  # checks
+  stopifnot(all(gsub(".*:.*:.*:(.*)", "\\1", sumstats$Name) == sumstats$A1 &
+                  all(gsub(".*:.*:(.*):.*", "\\1", sumstats$Name) == sumstats$A2)))
+  return(sumstats)
+}
+
