e142b3236b47b28087ad23b3a4b3e0de15a31251
Oleg Borisov <oleg.borisov@uniklinik-freiburg.de>
2023-08-25 15:38:36 +0200

    Add N to query GTExV8

--- R/development.R
+++ R/development.R
@@ -6,6 +6,7 @@
 # - Add input option for sdY
 # - Add to the file with internal examples
 # - Add colClasses, e.g., `colClasses = c(alt = "character", ref= "character")`
+# - Check there are no NAs in betas
 
 #' @title Query finngen GWAS
 #' @description Query finngen GWAS data to extract a region of interest
@@ -94,8 +95,8 @@ query_GTEXv8_GWAS <- function(sumstats_file,
   # format
   sumstats$Name <- paste0(sumstats$V1, ":", sumstats$V4, ":", sumstats$V5, ":", sumstats$V6)
   sumstats$rsids<- NA
-  sumstats <- sumstats[,c("Name", "rsids", "V19", "V4", "V6", "V5", "V15", "V16", "V14", "V11", "V8")]
-  colnames(sumstats) <- c("Name", "rsID", "CHR", "POS", "A1", "A2", "BETA", "SE", "P", "AF", "Phenotype")
+  sumstats <- sumstats[,c("Name", "rsids", "V19", "V4", "V6", "V5", "V15", "V16", "V14", "V11", "V12", "V8")]
+  colnames(sumstats) <- c("Name", "rsID", "CHR", "POS", "A1", "A2", "BETA", "SE", "P", "AF", "N", "Phenotype")
   # checks
   stopifnot(all(gsub(".*:.*:.*:(.*)", "\\1", sumstats$Name) == sumstats$A1 &
                   all(gsub(".*:.*:(.*):.*", "\\1", sumstats$Name) == sumstats$A2)))
