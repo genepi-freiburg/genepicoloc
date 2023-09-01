7a04304714c7b3b99a4824b47c3626ceb0f89e0a
Oleg Borisov <oleg.borisov@uniklinik-freiburg.de>
2023-09-01 14:13:02 +0200

    Keep query function for public studies only

--- R/01_query_sumstats.R
+++ R/01_query_sumstats.R
@@ -1,67 +1,7 @@
-# TODO
-# p_threshold p-value to include sumstats in coloc, default 1e-6
-# maf allele frequency to filter out SNPs, default 0.01
-# checks: first allele is reference
-# checks: duplicated SNPs
-# checks: A1 is alternative
-# coloc: add sdY
-
-#' @title Query GCKD pGWAS
-#' @description Query GCKD pGWAS data to extract a region of interest
-#' @param sumstats_file path to GCKD Olink sumstats.
-#' @param CHR_var CHR (as.character "1", "2", ..., "X").
-#' @param BP_START_var start of region, integer
-#' @param BP_STOP_var end of region, integer
-#' @return data frame with extracted sumstats
-#' @export
-query_GCKD_pGWAS <- function(sumstats_file, CHR_var, BP_START_var, BP_STOP_var) {
-  sumstats <- read.table(text=system(paste0("tabix -h ", sumstats_file, " ",
-                                            CHR_var, ":", BP_START_var, "-",
-                                            BP_STOP_var), intern = T), header = T)
-  if (nrow(sumstats) == 0) { return(NA) }
-  # format
-  sumstats$rsID <- NA
-  sumstats <- sumstats[,c("SNP", "rsID", "chr", "position", "noncoded_all", "coded_all", "beta", "SE", "pval", "AF_coded_all", "n_total")]
-  colnames(sumstats) <- c("Name", "rsID", "CHR", "POS", "A1", "A2", "BETA", "SE", "P", "AF", "N")
-  sumstats$BETA <- -sumstats$BETA
-  sumstats$AF <- 1 - sumstats$AF
-  # checks
-  stopifnot(all(gsub(".*:.*:.*:(.*)", "\\1", sumstats$Name) == sumstats$A1 &
-                  all(gsub(".*:.*:(.*):.*", "\\1", sumstats$Name) == sumstats$A2)))
-  # output
-  return(sumstats)
-}
-
-#' @title Query GCKD mGWAS plasma or urine
-#' @description Query GCKD mGWAS plasma or urine data to extract a region of interest
-#' @param sumstats_file path to sumstats.
-#' @param CHR_var CHR (as.character "1", "2", ..., "X").
-#' @param BP_START_var start of region, integer
-#' @param BP_STOP_var end of region, integer
-#' @return data frame with extracted sumstats
-#' @export
-query_GCKD_mGWAS <- function(sumstats_file,
-                             CHR_var, BP_START_var, BP_STOP_var) {
-  sumstats <- read.table(text=system(paste0("tabix -h ", sumstats_file, " ",
-                                            CHR_var, ":", BP_START_var, "-",
-                                            BP_STOP_var), intern = T), header = T)
-  if (nrow(sumstats) == 0) { return(NA) }
-  # format
-  sumstats$rsID <- NA
-  sumstats$P <- 10^(-sumstats$LOG10P)
-  sumstats <- sumstats[,c("ID", "rsID", "CHROM", "GENPOS", "ALLELE1", "ALLELE0", "BETA", "SE", "P", "A1FREQ", "N")]
-  colnames(sumstats) <- c("Name", "rsID", "CHR", "POS", "A1", "A2", "BETA", "SE", "P", "AF", "N")
-  # checks
-  stopifnot(all(gsub(".*:.*:.*:(.*)", "\\1", sumstats$Name) == sumstats$A1 &
-                  all(gsub(".*:.*:(.*):.*", "\\1", sumstats$Name) == sumstats$A2)))
-  return(sumstats)
-}
-
 #' @title Query UKB GWAS
 #' @description Query UK Biobank GWAS data to extract a region of interest
-#' @param sumstats_file path to UKB sumstats.
-#' @param annotation_file annotation file with number of samples for each GWAS.
-#' @param CHR_var CHR (as.character "1", "2", ..., "X").
+#' @param sumstats_file path tabix-indexed sumstats.
+#' @param CHR_var chromosome (as.character "1", "2", ..., "X").
 #' @param BP_START_var start of region, integer
 #' @param BP_STOP_var end of region, integer
 #' @return data frame with extracted sumstats.
@@ -84,10 +24,8 @@ query_UKB_GWAS <- function(sumstats_file,
 
 #' @title query Ferkingstad pGWAS
 #' @description Query Ferkingstad pGWAS (FpG) data to extract
-#' every pQTL region according to GCKD
-#'
-#' @param sumstats_file path to sumstats sumstats.
-#' @param CHR_var CHR (as.character "1", "2", ..., "X").
+#' @param sumstats_file path tabix-indexed sumstats.
+#' @param CHR_var chromosome (as.character "1", "2", ..., "X").
 #' @param BP_START_var start of region, integer
 #' @param BP_STOP_var end of region, integer
 #' @param assocvariants_annotate_file FpG file with corrected maf and other QC
@@ -120,8 +58,8 @@ query_Ferkingstad_pGWAS <- function(sumstats_file,
 
 #' @title query UKB PPP pGWAS
 #' @description Query UKB PPP pGWAS data to extract a region of interest
-#' @param sumstats_file path to sumstats.
-#' @param CHR_var CHR (as.character "1", "2", ..., "X").
+#' @param sumstats_file path tabix-indexed sumstats.
+#' @param CHR_var chromosome (as.character "1", "2", ..., "X").
 #' @param BP_START_var start of region, integer
 #' @param BP_STOP_var end of region, integer
 #' @return data frame with extracted sumstats
@@ -147,8 +85,8 @@ query_UKB_PPP_EUR <- function(sumstats_file,
 
 #' @title query ARIC pGWAS
 #' @description Query ARIC pGWAS data to extract a region of interest
-#' @param sumstats_file path to sumstats.
-#' @param CHR_var CHR (as.character "1", "2", ..., "X").
+#' @param sumstats_file path tabix-indexed sumstats.
+#' @param CHR_var chromosome (as.character "1", "2", ..., "X").
 #' @param BP_START_var start of region, integer
 #' @param BP_STOP_var end of region, integer
 #' @return data frame with extracted sumstats
@@ -181,8 +119,8 @@ query_ARIC_pGWAS <- function(sumstats_file,
 
 #' @title query CKD Gen
 #' @description Query CKD Gen data to extract a region of interest
-#' @param sumstats_file path to sumstats.
-#' @param CHR_var CHR (as.character "1", "2", ..., "X").
+#' @param sumstats_file path tabix-indexed sumstats.
+#' @param CHR_var chromosome (as.character "1", "2", ..., "X").
 #' @param BP_START_var start of region, integer
 #' @param BP_STOP_var end of region, integer
 #' @return data frame with extracted sumstats
@@ -206,60 +144,101 @@ query_CKD_pGWAS <- function(sumstats_file,
   return(sumstats)
 }
 
-#' @title query TAS
-#' @description Query Tricuspid Aortic Stenosis data to extract a region of interest
-#' @param sumstats_file path to sumstats.
-#' @param CHR_var CHR (as.character "1", "2", ..., "X").
+#' @title Query finngen GWAS
+#' @description Query finngen GWAS data to extract a region of interest
+#' @param sumstats_file path tabix-indexed sumstats.
+#' @param CHR_var chromosome (as.character "1", "2", ..., "X").
 #' @param BP_START_var start of region, integer
 #' @param BP_STOP_var end of region, integer
-#' @return data frame with extracted sumstats
+#' @return data frame with extracted sumstats.
+#' @examples
+#' query_finngen_GWAS(sumstats_file = "finngen_R9_E4_DM2REN.gz", CHR_var = "1", BP_START_var = 100000, BP_STOP_var = 110000)
 #' @export
-query_TAS <- function(sumstats_file,
-                      CHR_var, BP_START_var, BP_STOP_var) {
+query_finngen_GWAS <- function(sumstats_file,
+                               CHR_var, BP_START_var, BP_STOP_var) {
+  if (CHR_var == "X") {CHR_var <- 23}
   sumstats <- read.delim(text=system(paste0("tabix -h ", sumstats_file, " ",
                                             CHR_var, ":", BP_START_var, "-",
-                                            BP_STOP_var),
-                                     intern = T), header = T)
-  if (nrow(sumstats) == 0) { return(sumstats) }
+                                            BP_STOP_var), intern = T),  header = T, stringsAsFactors = FALSE, 
+                         colClasses = c(alt = "character", ref= "character"))
+  
+  if (nrow(sumstats) == 0) { return(NA) }
   # format
-  sumstats$rsID <- sumstats$ID
-  sumstats$Name <- paste0("chr", sumstats$CHR, ":", sumstats$BP, ":", sumstats$Other_allele, ":", sumstats$Effect_Allele)
-  sumstats$AF <- NA
-  sumstats$N <- NA
-  sumstats <- sumstats[,c("Name", "ID", "CHR", "BP", "Effect_Allele", "Other_allele", "Beta", "SE", "P", "AF", "N")]
-  colnames(sumstats) <- c("Name", "rsID", "CHR", "POS", "A1", "A2", "BETA", "SE", "P", "AF", "N")
+  sumstats$X.chrom[sumstats$X.chrom == 23] <- "X"
+  sumstats$Name <- paste0("chr", sumstats$X.chrom, ":", sumstats$pos, ":", sumstats$ref, ":", sumstats$alt)
+  sumstats <- sumstats[,c("Name", "rsids", "X.chrom", "pos", "alt", "ref", "beta", "sebeta", "pval", "af_alt")]
+  colnames(sumstats) <- c("Name", "rsID", "CHR", "POS", "A1", "A2", "BETA", "SE", "P", "AF")
   # checks
-  # stopifnot(all(gsub(".*:.*:.*:(.*)", "\\1", sumstats$Name) == sumstats$A1 &
-  #                 all(gsub(".*:.*:(.*):.*", "\\1", sumstats$Name) == sumstats$A2)))
+  stopifnot(all(gsub(".*:.*:.*:(.*)", "\\1", sumstats$Name) == sumstats$A1 &
+                  all(gsub(".*:.*:(.*):.*", "\\1", sumstats$Name) == sumstats$A2)))
   return(sumstats)
 }
 
 
-#' @title query CAD
-#' @description Query Coronary Artery Disease data to extract a region of interest
-#' @param sumstats_file path to sumstats.
-#' @param CHR_var CHR (as.character "1", "2", ..., "X").
+#' @title Query GTEx v7 GWAS
+#' @description Query GTEx v7 GWAS data to extract a region of interest
+#' @param sumstats_file path tabix-indexed sumstats.
+#' @param CHR_var chromosome (as.character "1", "2", ..., "X").
 #' @param BP_START_var start of region, integer
 #' @param BP_STOP_var end of region, integer
-#' @return data frame with extracted sumstats
+#' @return data frame with extracted sumstats.
+#' #' @examples
+#' query_GTEXv7_GWAS(sumstats_file = "Whole_Blood.allpairs_tmp_CHR_BP_sorted.txt.gz", CHR_var = "1", BP_START_var = 1000000, 
+#' BP_STOP_var = 1010000, phenotype_id_var = "ENSG00000177757.1")
 #' @export
-query_CAD <- function(sumstats_file,
-                      CHR_var, BP_START_var, BP_STOP_var) {
+
+query_GTEXv7_GWAS <- function(sumstats_file,
+                              CHR_var, BP_START_var, BP_STOP_var,
+                              phenotype_id_var) {
   sumstats <- read.delim(text=system(paste0("tabix -h ", sumstats_file, " ",
                                             CHR_var, ":", BP_START_var, "-",
-                                            BP_STOP_var),
-                                     intern = T), header = T)
-  if (nrow(sumstats) == 0) { return(sumstats) }
-  sumstats$rsID <- sumstats$ID
-  sumstats$Name <- paste0("chr", sumstats$CHR, ":", sumstats$BP, ":", sumstats$Other_allele, ":", sumstats$Effect_allele)
+                                            BP_STOP_var), intern = T),  header = T,  stringsAsFactors = FALSE, 
+                         colClasses = c(alt = "character", ref= "character"))
+  
+  if (nrow(sumstats) == 0) { return(NA) }
   # format
-  sumstats$AF <- NA
-  sumstats$N <- NA
-  sumstats <- sumstats[,c("Name", "ID", "CHR", "BP", "Effect_allele", "Other_allele", "Beta", "SE", "P.value", "AF", "N")]
-  colnames(sumstats) <- c("Name", "rsID", "CHR", "POS", "A1", "A2", "BETA", "SE", "P", "AF", "N")
+  sumstats$Name <- paste0("chr", sumstats$chr, ":", sumstats$pos, ":", sumstats$ref, ":", sumstats$alt)
+  sumstats$rsids<- NA
+  sumstats <- sumstats[,c("Name", "rsids", "chr", "pos", "alt", "ref", "slope", "slope_se", "pval_nominal", 
+                          "maf", "ma_samples", "gene_id")]
+  colnames(sumstats) <- c("Name", "rsID", "CHR", "POS", "A1", "A2", "BETA", "SE", "P", "AF", "N", "Phenotype")
   # checks
-  # stopifnot(all(gsub(".*:.*:.*:(.*)", "\\1", sumstats$Name) == sumstats$A1 &
-  #                 all(gsub(".*:.*:(.*):.*", "\\1", sumstats$Name) == sumstats$A2)))
+  stopifnot(all(gsub(".*:.*:.*:(.*)", "\\1", sumstats$Name) == sumstats$A1 &
+                  all(gsub(".*:.*:(.*):.*", "\\1", sumstats$Name) == sumstats$A2)))
+  return(sumstats)
+}
+
+
+#' @title Query GTEx v8 GWAS
+#' @description Query GTEx v8 GWAS data to extract a region of interest
+#' @param sumstats_file path tabix-indexed sumstats.
+#' @param CHR_var chromosome (as.character "1", "2", ..., "X").
+#' @param BP_START_var start of region, integer
+#' @param BP_STOP_var end of region, integer
+#' @return data frame with extracted sumstats.
+#' #' #' @examples
+#' query_GTEXv8_GWAS(sumstats_file = "GTEx_V8.gz", CHR_var = "X", BP_START_var = 20000, 
+#' BP_STOP_var = 28000, phenotype_id_var = "ENSG00000167393.17")
+#' @export
+query_GTEXv8_GWAS <- function(sumstats_file,
+                              CHR_var, BP_START_var, BP_STOP_var,
+                              phenotype_id_var) {
+  sumstats <- read.delim(text=system(paste0("tabix -h ", sumstats_file, " chr",
+                                            CHR_var, ":", BP_START_var, "-",
+                                            BP_STOP_var), intern = T),  header = F,  stringsAsFactors = FALSE, 
+                         colClasses = c(V5 = "character", V6= "character"))
+  
+  if (nrow(sumstats) == 0) { return(NA) }
+  # phenotype ID
+  sumstats <- subset(sumstats, V8 == phenotype_id_var)
+  # format
+  sumstats$Name <- paste0(sumstats$V1, ":", sumstats$V4, ":", sumstats$V5, ":", sumstats$V6)
+  sumstats$rsids<- NA
+  sumstats <- sumstats[,c("Name", "rsids", "V19", "V4", "V6", "V5", "V15", "V16", "V14", "V11", "V12", "V8")]
+  colnames(sumstats) <- c("Name", "rsID", "CHR", "POS", "A1", "A2", "BETA", "SE", "P", "AF", "N", "Phenotype")
+  # checks
+  stopifnot(all(gsub(".*:.*:.*:(.*)", "\\1", sumstats$Name) == sumstats$A1 &
+                  all(gsub(".*:.*:(.*):.*", "\\1", sumstats$Name) == sumstats$A2)))
   return(sumstats)
 }
 
