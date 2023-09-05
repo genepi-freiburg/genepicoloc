ee9cb5e7836233af3258e56d294bedfc1e0a407e
Oleg Borisov <oleg.borisov@uniklinik-freiburg.de>
2023-09-05 20:29:24 +0200

    Add query_sumstats_1 function

--- R/01_query_sumstats.R
+++ R/01_query_sumstats.R
@@ -1,3 +1,27 @@
+#' @title Read sumstats 1
+#' @description Read sumstats 1
+#' @param sumstats_file path tabix-indexed sumstats.
+#' @param CHR_var chromosome (as.character "1", "2", ..., "X").
+#' @param BP_START_var start of region, integer
+#' @param BP_STOP_var end of region, integer
+#' @return data frame with extracted sumstats.
+#' @export
+query_sumstats_1 <- function(sumstats_file,
+                           CHR_var, BP_START_var, BP_STOP_var,
+                           ...,
+                           read_mode = "RDS") {
+  if (read_mode == "RDS") {
+    sumstats <- readRDS(sumstats_file)
+  }
+  if (read_mode == "read.csv") {
+    sumstats <- read.csv(sumstats_file)
+  }
+  sumstats <- subset(sumstats, CHR == CHR_var & POS >= BP_START_var & POS <= BP_STOP_var)
+  return(sumstats)
+}
+
+
+
 #' @title Query UKB GWAS
 #' @description Query UK Biobank GWAS data to extract a region of interest
 #' @param sumstats_file path tabix-indexed sumstats.
