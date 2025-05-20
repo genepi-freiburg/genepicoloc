014b5af14053a8db559d1f14331748a4b3608d33
oborisov <olegbor77@gmail.com>
2025-05-20 16:08:17 +0200

    Move check_bin to utils

--- R/get_coloc_regions.R
+++ R/get_coloc_regions.R
@@ -377,26 +377,3 @@ gc_bgzip_tabix <- function(sumstats,
   return(gz_file)
 }
 
-#' Check if a binary is available in the system path
-#'
-#' @param bin_name Name or path of the binary to check
-#' @return TRUE if the binary is available, otherwise stops with an error
-#' @keywords internal
-check_bin <- function(bin_name) {
-  # Use 'which' on Unix/Linux/Mac or 'where' on Windows
-  cmd <- if (.Platform$OS.type == "windows") {
-    paste0("where ", bin_name, " 2>NUL")
-  } else {
-    paste0("which ", bin_name, " 2>/dev/null")
-  }
-  
-  # Run command and check result
-  result <- system(cmd, intern = TRUE)
-  
-  if (length(result) == 0) {
-    stop("Required binary '", bin_name, "' not found in system path. ",
-         "Please install it or provide the full path.")
-  }
-  
-  return(TRUE)
-}
