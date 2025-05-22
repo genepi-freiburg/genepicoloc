2047a68e7c5c2d8941eb39d25d5f6b8529b364ea
Oleg Borisov <oleg.borisov@uniklinik-freiburg.de>
2025-05-22 15:53:22 +0200

    Add eQTL studies to query_sumstats_custom

--- R/query_sumstats_custom.R
+++ R/query_sumstats_custom.R
@@ -29,7 +29,10 @@ tabix_Icelanders_pGWAS <- function(sumstats_file, coloc_regions_PASS) {
 #' format_Icelanders_pGWAS
 format_Icelanders_pGWAS <- function(sumstats) {
   # check
-  cols_from <- c("Name", "Chrom", "Pos", "rsids", "effectAllele", "otherAllele", "Beta", "Pval", "minus_log10_pval", "SE", "N", "ImpMAF", "effectAlleleFreq")
+  cols_from <- c("Chrom", "Pos", "Name", "rsids", "effectAllele", "otherAllele", "Beta", "Pval", "minus_log10_pval", "SE", "N", "ImpMAF", "effectAlleleFreq")
+  if (!identical(cols_from, colnames(sumstats))) {
+    stop("Column mismatch when reading ", attr(sumstats, "sumstats_file"))
+  }
   # format
   sumstats$Chrom <- gsub("chr", "", sumstats$Chrom)
   sumstats$Name <- gsub("(.*:.*):(.*):(.*)", "\\1:\\3:\\2", sumstats$Name)
@@ -55,7 +58,9 @@ tabix_UKB_PPP_EUR <- function(sumstats_file, coloc_regions_PASS) {
   coloc_regions_PASS$CHR_var[coloc_regions_PASS$CHR_var == "X"] <- "23"
   sumstats <- retrieve_sumstats_tabix(sumstats_file=sumstats_file,
                              coloc_regions_PASS=coloc_regions_PASS)
-  sumstats <- format_UKB_PPP_EUR(sumstats=sumstats)
+  if (attr(sumstats, "tabix") != "tabix_failed") {
+    sumstats <- format_UKB_PPP_EUR(sumstats=sumstats)
+  }
   return(sumstats)
 }
 
@@ -63,6 +68,9 @@ tabix_UKB_PPP_EUR <- function(sumstats_file, coloc_regions_PASS) {
 format_UKB_PPP_EUR <- function(sumstats) {
   # check
   cols_from <- c("CHROM", "GENPOS", "ID", "ALLELE0", "ALLELE1", "A1FREQ", "INFO", "N", "TEST", "BETA", "SE", "CHISQ", "LOG10P", "EXTRA")
+  if (!identical(cols_from, colnames(sumstats))) {
+    stop("Column mismatch when reading ", attr(sumstats, "sumstats_file"))
+  }
   # format
   sumstats$rsID <- NA
   sumstats$CHROM[sumstats$CHROM == "23"] <- "X"
@@ -87,7 +95,9 @@ format_UKB_PPP_EUR <- function(sumstats) {
 tabix_UKB_TOPMed <- function(sumstats_file, coloc_regions_PASS) {
   sumstats <- retrieve_sumstats_tabix(sumstats_file=sumstats_file,
                              coloc_regions_PASS=coloc_regions_PASS)
-  sumstats <- format_UKB_TOPMed(sumstats=sumstats)
+  if (attr(sumstats, "tabix") != "tabix_failed") {
+    sumstats <- format_UKB_TOPMed(sumstats=sumstats)
+  }
   return(sumstats)
 }
 
@@ -95,6 +105,9 @@ tabix_UKB_TOPMed <- function(sumstats_file, coloc_regions_PASS) {
 format_UKB_TOPMed <- function(sumstats) {
   # check
   cols_from <- c("chrom", "pos", "ref", "alt", "rsids", "nearest_genes", "consequence", "pval", "beta", "sebeta", "af", "case_af", "control_af", "tstat")
+  if (!identical(cols_from, colnames(sumstats))) {
+    stop("Column mismatch when reading ", attr(sumstats, "sumstats_file"))
+  }
   # format
   sumstats$Name <- paste0("chr", sumstats$chrom, ":", sumstats$pos, ":", sumstats$ref, ":", sumstats$alt)
   sumstats$N <- NA
@@ -122,7 +135,9 @@ tabix_FinnGen_r9 <- function(sumstats_file, coloc_regions_PASS) {
   coloc_regions_PASS$CHR_var[coloc_regions_PASS$CHR_var == "X"] <- "23"
   sumstats <- retrieve_sumstats_tabix(sumstats_file=sumstats_file,
                              coloc_regions_PASS=coloc_regions_PASS)
-  sumstats <- format_FinnGen_r9(sumstats=sumstats)
+  if (attr(sumstats, "tabix") != "tabix_failed") {
+    sumstats <- format_FinnGen_r9(sumstats=sumstats)
+  }
   return(sumstats)
 }
 
@@ -130,6 +145,9 @@ tabix_FinnGen_r9 <- function(sumstats_file, coloc_regions_PASS) {
 format_FinnGen_r9 <- function(sumstats) {
   # check
   cols_from <- c("#chrom","pos","ref","alt","rsids","nearest_genes","pval","mlogp","beta","sebeta","af_alt","af_alt_cases","af_alt_controls")
+  if (!identical(cols_from, colnames(sumstats))) {
+    stop("Column mismatch when reading ", attr(sumstats, "sumstats_file"))
+  }
   # format
   sumstats[["#chrom"]][sumstats[["#chrom"]] == "23"] <- "X"
   sumstats$Name <- paste0("chr", sumstats[["#chrom"]], ":", sumstats$pos, ":", sumstats$ref, ":", sumstats$alt)
@@ -150,12 +168,105 @@ format_FinnGen_r9 <- function(sumstats) {
   return(sumstats)
 }
 
+#' tabix_GTEXv8
+tabix_GTEXv8 <- function(sumstats_file, coloc_regions_PASS) {
+  coloc_regions_PASS$CHR_var <- paste0("chr", coloc_regions_PASS$CHR_var)
+  sumstats <- retrieve_sumstats_tabix(sumstats_file=sumstats_file,
+                                      coloc_regions_PASS=coloc_regions_PASS,
+                                      sumstats_pheno="multiple")
+  # GTEXv8 does not have header, so if there are no lines
+  # it will return Null data.table (0 rows and 0 cols)
+  # which is handled as "tabix_failed"
+  # fix "tabix_failed" in case there was no error
+  tabix_cmd <- retrieve_sumstats_tabix(sumstats_file=sumstats_file,
+                                       coloc_regions_PASS=coloc_regions_PASS,
+                                       file_remove=FALSE,
+                                       return_tabix_cmd=TRUE)
+  file.remove(attr(tabix_cmd, "file_regions"))
+  if (identical(system(tabix_cmd, ignore.stdout=T), 0L)) {
+    attr(sumstats, "tabix") <- "tabix_ok_no_data"
+  }
+  if (attr(sumstats, "tabix") != "tabix_failed") {
+    sumstats <- format_GTEXv8(sumstats=sumstats)
+  }
+  return(sumstats)
+}
+
+#' format_GTEXv8
+format_GTEXv8 <- function(sumstats) {
+  # check
+  cols_from <- paste0("V", 1:19)
+  if (!identical(cols_from, colnames(sumstats))) {
+    stop("Column mismatch when reading ", attr(sumstats, "sumstats_file"))
+  }
+  # format
+  sumstats$nlog10P <- -log10(sumstats$V14)
+  sumstats$Name <- paste0(sumstats$V1, ":", sumstats$V4, ":", sumstats$V5, ":", sumstats$V6)
+  sumstats$rsids <- NA
+  # colnames
+  sumstats <- match_cols(sumstats=sumstats,
+                         Name="Name",
+                         rsID="rsids",
+                         CHR="V19",
+                         POS="V4",
+                         A1="V6",
+                         A2="V5",
+                         BETA="V15",
+                         SE="V16",
+                         nlog10P="nlog10P",
+                         AF="V11",
+                         N="V12",
+                         Phenotype="V8")
+  return(sumstats)
+}
+
+#' tabix_Kidney_eQTL
+tabix_Kidney_eQTL <- function(sumstats_file, coloc_regions_PASS) {
+  sumstats <- retrieve_sumstats_tabix(sumstats_file=sumstats_file,
+                                      coloc_regions_PASS=coloc_regions_PASS,
+                                      sumstats_pheno="multiple")
+  if (attr(sumstats, "tabix") != "tabix_failed") {
+    sumstats <- format_Kidney_eQTL(sumstats=sumstats)
+  }
+  return(sumstats)
+}
+
+#' format_Kidney_eQTL
+format_Kidney_eQTL <- function(sumstats) {
+  # check
+  cols_from <- c("CHR","POS_hg38","Name","GeneID","Gene_Symbol","rsID","SNP","Ref","Alt","Beta","Std","Pvalue","SNP_Location","Compartment")
+  if (!identical(cols_from, colnames(sumstats))) {
+    stop("Column mismatch when reading ", attr(sumstats, "sumstats_file"))
+  }
+  # format
+  sumstats$nlog10P <- -log10(sumstats$Pvalue)
+  sumstats$AF <- NA
+  sumstats$N <- NA
+  # colnames
+  sumstats <- match_cols(sumstats=sumstats,
+                         Name="Name",
+                         rsID="rsID",
+                         CHR="CHR",
+                         POS="POS_hg38",
+                         A1="Alt",
+                         A2="Ref",
+                         BETA="Beta",
+                         SE="Std",
+                         nlog10P="nlog10P",
+                         AF="AF",
+                         N="N",
+                         Phenotype="GeneID")
+  return(sumstats)
+}
+
 #' tabix_eQTLGen
 tabix_eQTLGen <- function(sumstats_file, coloc_regions_PASS) {
   sumstats <- retrieve_sumstats_tabix(sumstats_file=sumstats_file,
-                                      coloc_regions_PASS=coloc_regions_PASS)
-  attr(sumstats, "sumstats_str") <- "multiple"
-  sumstats <- format_eQTLGen(sumstats=sumstats)
+                                      coloc_regions_PASS=coloc_regions_PASS,
+                                      sumstats_pheno="multiple")
+  if (attr(sumstats, "tabix") != "tabix_failed") {
+    sumstats <- format_eQTLGen(sumstats=sumstats)
+  }
   return(sumstats)
 }
 
@@ -163,7 +274,9 @@ tabix_eQTLGen <- function(sumstats_file, coloc_regions_PASS) {
 format_eQTLGen <- function(sumstats) {
   # check
   cols_from <- c("Name","rsID","CHR","POS","A1","A2","BETA","SE","P","AF","N","Phenotype")
-  if (!identical(cols_from, colnames(sumstats))) stop("Column mismatch when reading eQTLGen")
+  if (!identical(cols_from, colnames(sumstats))) {
+    stop("Column mismatch when reading ", attr(sumstats, "sumstats_file"))
+  }
   # format
   sumstats$nlog10P <- -log10(sumstats$P)
   # colnames
